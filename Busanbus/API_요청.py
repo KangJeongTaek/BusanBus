f20e52640e5f854339d8495fe97203ce917d2fc3
KangJeongTaek <dfgddjd@naver.com>
2024-03-13 22:42:17 +0900

    데이터 정제

--- Busanbus/API_요청.py
+++ Busanbus/API_요청.py
@@ -17,68 +17,63 @@ def busStop ():
     response = requests.get(url, params=params)
     decodecontent = response.content.decode('utf-8') 
     xmlParser = BeautifulSoup(decodecontent,'xml')
-    contents =  xmlParser.find_all('item')
-    busStopInfo= []
-    for content in contents:
-        bstopid = content.find('bstopid')
-        bstopnm = content.find('bstopnm')
-        arsno = content.find('arsno')
-        gpsx = content.find('gpsx')
+    contents =  xmlParser.select_one('items').get_text().strip()
+    content = contents.split('\n\n\n')
+    print(content[0])
 
-    
-    ## 1번 인덱스 = 정류소 아이디
-    ## 2번 인덱스 = 정류소 명
-    ## 3번 인덱스 = 정류소 번호
-    ## 4번 인덱스,5번 인덱스 = GPS X 좌표, GPS Y 좌표
-    ## 6번 인덱스 = 정류장 구분
-    return busStopInfo
+    ## content[0] == 요청한 첫번 째 정보
+    ## 정류소 아이디
+    ## 정류소 명
+    ## 정류소 번호
+    ## GPS X좌표, GPS Y좌표
+    ## 정류장 구분 순으로 데이터 정제
 
 
-print(busStop())
+busStop()
 
 
 # 버스 번호로 노선 검색하기
-def busLine():
-    url = 'http://apis.data.go.kr/6260000/BusanBIMS/busInfo'
-    params ={'serviceKey' : 'l2SER5UdFnVMTCfW/75Op1w1xj0ZVJyWjxZCA4s2qcMkJIjXonsQeorj8k35mmylOV0ywqc75RoXXtagwXuUnQ==', 
-            'lineno' : '179' }
+# def busLine():
+#     url = 'http://apis.data.go.kr/6260000/BusanBIMS/busInfo'
+#     params ={'serviceKey' : 'l2SER5UdFnVMTCfW/75Op1w1xj0ZVJyWjxZCA4s2qcMkJIjXonsQeorj8k35mmylOV0ywqc75RoXXtagwXuUnQ==', 
+#             'lineno' : '179' }
 
-    response = requests.get(url, params=params)
-    decodecontent = response.content.decode('utf-8') 
-    xmlParser = BeautifulSoup(decodecontent,'xml')
-    contents =  xmlParser.find('items')
+#     response = requests.get(url, params=params)
+#     decodecontent = response.content.decode('utf-8') 
+#     xmlParser = BeautifulSoup(decodecontent,'xml')
+#     contents =  xmlParser.find('items')
 
-    return contents
+#     return contents
 
-# print(busLine())
+# # print(busLine())
 
 
 # 노선 ID를 통한 노선 정보 및 실시간 버스 위치 정보 제공
-def buslineId():
-    url = 'http://apis.data.go.kr/6260000/BusanBIMS/busInfoByRouteId'
-    params ={'serviceKey' : 'l2SER5UdFnVMTCfW/75Op1w1xj0ZVJyWjxZCA4s2qcMkJIjXonsQeorj8k35mmylOV0ywqc75RoXXtagwXuUnQ==',
-             'lineid' : '5200179000' }
+# def buslineId():
+#     url = 'http://apis.data.go.kr/6260000/BusanBIMS/busInfoByRouteId'
+#     params ={'serviceKey' : 'l2SER5UdFnVMTCfW/75Op1w1xj0ZVJyWjxZCA4s2qcMkJIjXonsQeorj8k35mmylOV0ywqc75RoXXtagwXuUnQ==',
+#              'lineid' : '5200179000' }
 
-    response = requests.get(url, params=params)
-    decodecontent = response.content.decode('utf-8') 
-    xmlParser = BeautifulSoup(decodecontent,'xml')
-    contents =  xmlParser.find_all('item')
-    # 1번 인덱스 방향
-    # 2번 인덱스 
+#     response = requests.get(url, params=params)
+#     decodecontent = response.content.decode('utf-8') 
+#     xmlParser = BeautifulSoup(decodecontent,'xml')
+#     contents =  xmlParser.find_all('item')
+#     # 1번 인덱스 방향
+#     # 2번 인덱스 
 
-    return contents
+#     return contents
 
-# print(buslineId())
+# # print(buslineId())
 
-# 정류소ID를 검색조건으로 실시간 버스 도착정보를 제공
-def busArrive():
-    url = 'http://apis.data.go.kr/6260000/BusanBIMS/stopArrByBstopid'
-    params ={'serviceKey' : 'l2SER5UdFnVMTCfW/75Op1w1xj0ZVJyWjxZCA4s2qcMkJIjXonsQeorj8k35mmylOV0ywqc75RoXXtagwXuUnQ==',
-             'bstopid' : '505780000' }
+# # 정류소ID를 검색조건으로 실시간 버스 도착정보를 제공
+# def busArrive():
+#     url = 'http://apis.data.go.kr/6260000/BusanBIMS/stopArrByBstopid'
+#     params ={'serviceKey' : 'l2SER5UdFnVMTCfW/75Op1w1xj0ZVJyWjxZCA4s2qcMkJIjXonsQeorj8k35mmylOV0ywqc75RoXXtagwXuUnQ==',
+#              'bstopid' : '505780000' }
 
-    response = requests.get(url, params=params)
-    decodecontent = response.content.decode('utf-8') 
-    xmlParser = BeautifulSoup(decodecontent,'xml')
-    return xmlParser
+#     response = requests.get(url, params=params)
+#     decodecontent = response.content.decode('utf-8') 
+#     xmlParser = BeautifulSoup(decodecontent,'xml')
+#     return xmlParser
 
-# print(busArrive())
+# # print(busArrive())
